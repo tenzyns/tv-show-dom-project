718d1cd22f407a4ba394497d4770cdb7146d66e3
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-25 23:05:16 +0100

    level 100 completed

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-
   const rootElem = document.getElementById("root");
+
+  //extracting each of the episode data
   allEpisodes.forEach(episode => {
     const cardEl = document.createElement("article");
     cardEl.className = "card";
-    
     rootElem.appendChild(cardEl);
 
-   let headingEl = document.createElement("h2");
-    headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}
-    ${episode.name}`;
+   const headingEl = document.createElement("h2");
+   //getting episode and season number with formatted double digits
+    headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
     
-    let thumbNail = document.createElement("img");
+    //anchor tag to link back to TVmaze for credit
+    const urlEpisode = document.createElement("a");
+    urlEpisode.href = episode.url;
+    urlEpisode.target = "_blank";
+
+    //episode image attached to anchor tag
+    const thumbNail = document.createElement("img");
     thumbNail.src = episode.image.medium;
     thumbNail.alt = "episode thumbnail";
     thumbNail.className = "thumb-nail";
+    urlEpisode.appendChild(thumbNail);
+
+    //episode summary
+    const synopsis = document.createElement("div");
+    synopsis.innerHTML = episode.summary;
+    cardEl.appendChild(headingEl);
+    cardEl.appendChild(urlEpisode);
+    cardEl.appendChild(synopsis);
 
     
-    cardEl.appendChild(headingEl);
-    cardEl.appendChild(thumbNail);
-    //cardEl.innerHTML = episode.summary;
     });
 }
 
