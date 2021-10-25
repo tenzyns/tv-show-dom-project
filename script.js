5faeb576fa55d58c259bf6c07949df1c0e6162c5
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-25 19:41:24 +0100

    basic cards of episode addded

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+
+  const rootElem = document.getElementById("root");
+  allEpisodes.forEach(episode => {
+    const cardEl = document.createElement("article");
+    cardEl.className = "card";
+    
+    rootElem.appendChild(cardEl);
+
+   let headingEl = document.createElement("h2");
+    headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}
+    ${episode.name}`;
+    
+    let thumbNail = document.createElement("img");
+    thumbNail.src = episode.image.medium;
+    thumbNail.alt = "episode thumbnail";
+    thumbNail.className = "thumb-nail";
+
+    
+    cardEl.appendChild(headingEl);
+    cardEl.appendChild(thumbNail);
+    //cardEl.innerHTML = episode.summary;
+    });
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.textContent = `Got why ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
