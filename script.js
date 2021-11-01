ebf307402fc46c44c30ae8c63ecdb799eb4ae0e4
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-11-01 20:51:39 +0000

    fetch function used to extract tv data from tvmaze.com

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
 
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  const rootElem = document.getElementById("root");
 
-  //extracting each of the episode data
-  allEpisodes.forEach(episode => {
-    const cardEl = document.createElement("article");
-    cardEl.className = "card";
-    cardEl.setAttribute("id", episode.id.toString());
-    rootElem.appendChild(cardEl);
+  fetch("https://api.tvmaze.com/shows/82/episodes")
+    .then(function(response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw `${response.status} ${response.statusText}`;
+    })
+    .then(function(data) {
 
-    const headingEl = document.createElement("h2");
-   //getting episode and season number with formatted double digits
-    headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
-    
-    //anchor tag to link back to TVmaze for credit
-    const urlEpisode = document.createElement("a");
-    urlEpisode.href = episode.url;
-    urlEpisode.target = "_blank";
+      const rootElem = document.getElementById("root");
 
-    //episode image attached to anchor tag
-    const thumbNail = document.createElement("img");
-    thumbNail.src = episode.image.medium;
-    thumbNail.alt = `episode thumbnail for ${episode.name}`;
-    thumbNail.className = "thumb-nail";
-    urlEpisode.appendChild(thumbNail);
+      //extracting each of the episode data
+      data.forEach(episode => {
+        const cardEl = document.createElement("article");
+        cardEl.className = "card";
+        cardEl.setAttribute("id", episode.id.toString());
+        rootElem.appendChild(cardEl);
 
-    //episode summary
-    const synopsis = document.createElement("div");
-    synopsis.innerHTML = episode.summary;
-    cardEl.appendChild(headingEl);
-    cardEl.appendChild(urlEpisode);
-    cardEl.appendChild(synopsis);
+        const headingEl = document.createElement("h2");
+      //getting episode and season number with formatted double digits
+        headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
+        
+        //anchor tag to link back to TVmaze for credit
+        const urlEpisode = document.createElement("a");
+        urlEpisode.href = episode.url;
+        urlEpisode.target = "_blank";
 
-    
-  });
+        //episode image attached to anchor tag
+        const thumbNail = document.createElement("img");
+        thumbNail.src = episode.image.medium;
+        thumbNail.alt = `episode thumbnail for ${episode.name}`;
+        thumbNail.className = "thumb-nail";
+        urlEpisode.appendChild(thumbNail);
+
+        //episode summary
+        const synopsis = document.createElement("div");
+        synopsis.innerHTML = episode.summary;
+        cardEl.appendChild(headingEl);
+        cardEl.appendChild(urlEpisode);
+        cardEl.appendChild(synopsis);            
+      });     
+         
+      //---Select and option elements----
+  
+      data.forEach(episode => {
+        let optEl = document.createElement("option");
+        optEl.value = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
+        optEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
+        let anchorEl = document.createElement("a");
+        anchorEl.href = `${episode.id}`; //episode id is assigned as href attribute for navigation
+        optEl.appendChild(anchorEl);
+        let selectEl = document.getElementById("select");
+        selectEl.appendChild(optEl);
+        
+        selectEl.addEventListener("change", showEpisode);
+        function showEpisode() {
+          let selectedValue = selectEl.options[selectEl.selectedIndex].value;
+          if (selectedValue === `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`) {
+            window.location.href = `#${episode.id}`;
+          } 
+        }
+      });  
+    })
+    .catch(function(error) {
+        return "An error occurred: " + error;
+    });
 
   //---------Search box ------------
   const searchBox = document.getElementById("search");
@@ -54,46 +86,21 @@ function setup() {
       if (episodeText.toUpperCase().includes(searchTerm)|| summaryText.toUpperCase().indexOf(searchTerm) > -1) {
         allCards[i].style.display = "";
         resultCount++;        
-        } else {
-          allCards[i].style.display = "none";          
-        }
-        //Shows search result count
-        spanEl.innerHTML = `Showing ${resultCount}/${allEpisodes.length} episodes`;
-        
-        //if search box is clear, count result text disappears
-        if (searchTerm === "") {
-          spanEl.style.display = "none";
-        } else {
-          spanEl.style.display = "inline";
-        }
-    }
+      } else {
+        allCards[i].style.display = "none";          
+      }
+      //Shows search result count
+      spanEl.innerHTML = `Showing ${resultCount}/${allCards.length} episodes`;
+      
+      //if search box is clear, count result text disappears
+      if (searchTerm === "") {
+        spanEl.style.display = "none";
+      } else {
+        spanEl.style.display = "inline";
+      }
+    }  
     
-    
-  }  
-  //---Select and option elements----
+  }
   
-  allEpisodes.forEach(episode => {
-    let optEl = document.createElement("option");
-    optEl.value = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
-    optEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
-    let anchorEl = document.createElement("a");
-    anchorEl.href = `${episode.id}`; //episode id is assigned as href attribute for navigation
-    optEl.appendChild(anchorEl);
-    let selectEl = document.getElementById("select");
-    selectEl.appendChild(optEl);
-    
-    selectEl.addEventListener("change", showEpisode);
-    function showEpisode() {
-      let selectedValue = selectEl.options[selectEl.selectedIndex].value;
-      if (selectedValue === `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`) {
-        window.location.href = `#${episode.id}`;
-      } 
-    }
-    });
-  
-
 }
-
-  
-
 window.onload = setup;
