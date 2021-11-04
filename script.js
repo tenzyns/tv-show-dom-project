467782199fe4640dd211afc717a9240daab59fce
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-11-04 19:19:34 +0000

    API call added so that a selected tv show's episodes can be viewed

--- script.js
+++ script.js
@@ -1,46 +1,98 @@
 
 function setup() {
+  const allShow = getAllShows();
+  console.log(allShow.length);
+  makePageForEpisodes(allShow);
 
+}
+
+function makePageForEpisodes(showList) {
+
+  // select element for picking a show
+  const showDiv = document.getElementById("show-list");
+  const selectShowEl = document.createElement("select");
+  selectShowEl.style.textAlignLast = "center";
+  selectShowEl.id = "allShows";
+  const option = document.createElement("option");
+  option.value = "";
+  option.textContent = "--Select a TV show--";
+  option.selected = "selected";
+  showDiv.appendChild(selectShowEl);
+  selectShowEl.appendChild(option);
+
+  //----List of all shows under selector----
+  showList.forEach(show => {
+    const optionEl = document.createElement("option");
+    optionEl.value = show.name;
+    optionEl.innerText = show.name;
+    selectShowEl.appendChild(optionEl);
+
+    let showId = show.id;
+    let showApi = `https://api.tvmaze.com/shows/${showId}/episodes`;
+    
+    //EventListener for selected show
+    selectShowEl.addEventListener("change", showAllEpisodes);
+
+    function showAllEpisodes() {
+      let selectedValue = selectShowEl.options[selectShowEl.selectedIndex].value;
+      if (selectedValue === show.name) {
+        fetch(showApi)
+        .then((response) => response.json())
+        .then((data) => {
+        
+          const rootElem = document.getElementById("root");
+
+          //extracting each of the episode data
+          data.forEach(episode => {
+            const cardEl = document.createElement("article");
+            cardEl.className = "card";
+            cardEl.setAttribute("id", episode.id.toString());
+            rootElem.appendChild(cardEl);
+
+            const headingEl = document.createElement("h2");
+            //getting episode and season number with formatted double digits
+            headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
+            
+            //anchor tag to link back to TVmaze for credit
+            const urlEpisode = document.createElement("a");
+            urlEpisode.href = episode.url;
+            urlEpisode.target = "_blank";
+
+            //episode image attached to anchor tag
+            const thumbNail = document.createElement("img");
+            thumbNail.src = episode.image.medium;
+            thumbNail.alt = `episode thumbnail for ${episode.name}`;
+            thumbNail.className = "thumb-nail";
+            urlEpisode.appendChild(thumbNail);
+
+            //episode summary
+            const synopsis = document.createElement("div");
+            synopsis.innerHTML = episode.summary;
+            cardEl.appendChild(headingEl);
+            cardEl.appendChild(urlEpisode);
+            cardEl.appendChild(synopsis);  
+
+
+          });
+        })
+        .catch((err) => console.log(err));
+
+        
+      }
+    }
+  });
+
+}
+/*
   fetch("https://api.tvmaze.com/shows/82/episodes")
     .then(function(response) {
         if (response.ok) {
-            return response.json();
+          return response.json();
         }
         throw `${response.status} ${response.statusText}`;
     })
     .then(function(data) {
-
-      const rootElem = document.getElementById("root");
-
-      //extracting each of the episode data
-      data.forEach(episode => {
-        const cardEl = document.createElement("article");
-        cardEl.className = "card";
-        cardEl.setAttribute("id", episode.id.toString());
-        rootElem.appendChild(cardEl);
-
-        const headingEl = document.createElement("h2");
-      //getting episode and season number with formatted double digits
-        headingEl.textContent = `S${String(episode.season).padStart(2, 0)}E${String(episode.number).padStart(2, 0)}: ${episode.name}`;
-        
-        //anchor tag to link back to TVmaze for credit
-        const urlEpisode = document.createElement("a");
-        urlEpisode.href = episode.url;
-        urlEpisode.target = "_blank";
-
-        //episode image attached to anchor tag
-        const thumbNail = document.createElement("img");
-        thumbNail.src = episode.image.medium;
-        thumbNail.alt = `episode thumbnail for ${episode.name}`;
-        thumbNail.className = "thumb-nail";
-        urlEpisode.appendChild(thumbNail);
-
-        //episode summary
-        const synopsis = document.createElement("div");
-        synopsis.innerHTML = episode.summary;
-        cardEl.appendChild(headingEl);
-        cardEl.appendChild(urlEpisode);
-        cardEl.appendChild(synopsis);            
+          
       });     
          
       //---Select and option elements----
@@ -100,7 +152,9 @@ function setup() {
       }
     }  
     
-  }
-  
 }
+
+  }*/
+  
+
 window.onload = setup;
